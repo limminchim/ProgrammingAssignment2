2875538c464120124641c77412a3cb593977ed28
Lim Min Chim <limmc@i2r.a-star.edu.sg>
2014-09-16 23:26:07 +0800

    Add some comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,53 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## R function to cache potentially time-consuming computations
 makeCacheMatrix <- function(x = matrix()) {
     
-        ## my first editing
-        print(x)
+
 
 }
 
-
 ## Write a short comment describing this function
-
+## R function to Calculates the inverse of a matrix
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
-        print(x)
+        
+}
+
+## Creates a special "vector", which is really a list containing a function to
+##    - set the value of the vector
+##    - get the value of the vector
+##    - set the value of the mean
+##    - get the value of the mean
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+## Calculates the mean of the special "vector" created with the above function. 
+## - First checks to see if the mean has already been calculated. 
+## - If yes, it gets the mean from the cache and skips the computation. 
+## - If no, it calculates the mean of the data and sets the value of the mean 
+##      in the cache via the setmean function.
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
